8cb33b559c510776d4e519f5b37bd3d06be13f54
nayla <naylahusna@Naylas-MacBook-Air-2.local>
2026-01-08 16:02:27 +0700

    menambah while loop

--- tugas.py
+++ tugas.py
@@ -8,27 +8,29 @@
 
 secret_word = "mangga"
 # step 1
-input_user = input("Please input the letter : ")
+while True: 
+    input_user = input("Please input the letter : ").lower()
 
-length_input = len(input_user)
-# step 2
-if length_input == 1:
-    # step 3
-    # input pasti satu karakter
-    is_alphabet_lower = input_user >= "a" and input_user <= "z"
-    is_alphabet_upper = input_user >= "A" and input_user <= "Z"
-    is_alphabet = is_alphabet_lower or is_alphabet_upper
-    if is_alphabet == True:
-        # step 4 , ignore capitalizaiton, force to lower case
-        secret_word = secret_word.lower()
-        input_user = input_user.lower()
-        # step 5, check if the letter is in secret_word
-        if input_user in secret_word:
-            print(" -> Your answer is correct! . . .")
-        else : 
-            print(" -> Unfortunately your answer is wrong!")
+    length_input = len(input_user)
+    # step 2
+    if length_input == 1:
+        # step 3
+        # input pasti satu karakter
+        is_alphabet_lower = input_user >= "a" and input_user <= "z"
+        is_alphabet_upper = input_user >= "A" and input_user <= "Z"
+        is_alphabet = is_alphabet_lower or is_alphabet_upper
+        if is_alphabet == True:
+            # step 4 , ignore capitalizaiton, force to lower case
+            secret_word = secret_word.lower()
+            input_user = input_user.lower()
+            # step 5, check if the letter is in secret_word
+            if input_user in secret_word:
+                print(" -> Your answer is correct! . . .")
+                break
+            else : 
+                print(" -> Unfortunately your answer is wrong!")
+        else :
+            print(" -> Input is not a valid alphabet!")
     else :
-        print(" -> Input is not a valid alphabet!")
-else :
-    print(" -> Please input only one letter!")
+        print(" -> Please input only one letter!")
 
