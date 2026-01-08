799980297263f8c34d504b773ec265270c25682b
nayla <naylahusna@Naylas-MacBook-Air-2.local>
2026-01-08 16:30:52 +0700

    LabPertama_withKakaNoelUpdate

--- LabPertama_withKakaNoelGatnteng.py
+++ LabPertama_withKakaNoelGatnteng.py
@@ -3,27 +3,18 @@
 # 3. pastikan user hanya menginput huruf 
 # 4. memastikan karakter tersebut ada di secret_word regardless capitalization
 
-# a - z dan A - Z
-
 
 secret_word = "mangga"
-# step 1
 while True: 
     input_user = input("Please input the letter : ").lower()
-
     length_input = len(input_user)
-    # step 2
     if length_input == 1:
-        # step 3
-        # input pasti satu karakter
         is_alphabet_lower = input_user >= "a" and input_user <= "z"
         is_alphabet_upper = input_user >= "A" and input_user <= "Z"
         is_alphabet = is_alphabet_lower or is_alphabet_upper
         if is_alphabet == True:
-            # step 4 , ignore capitalizaiton, force to lower case
             secret_word = secret_word.lower()
             input_user = input_user.lower()
-            # step 5, check if the letter is in secret_word
             if input_user in secret_word:
                 print(" -> Your answer is correct! . . .")
                 break
--- lab1.py
+++ lab1.py
@@ -1,29 +1,14 @@
-# 1. minta buat kata secret_word
-# 2. user memasukan 1 karakter
-# 3. pastikan user hanya menginput huruf 
-# 4. memastikan karakter tersebut ada di secret_word regardless capitalization
-
-# a - z dan A - Z
-
-
 secret_word = "mangga"
-# step 1
 while True: 
     input_user = input("Please input the letter : ").lower()
-
     length_input = len(input_user)
-    # step 2
     if length_input == 1:
-        # step 3
-        # input pasti satu karakter
         is_alphabet_lower = input_user >= "a" and input_user <= "z"
         is_alphabet_upper = input_user >= "A" and input_user <= "Z"
         is_alphabet = is_alphabet_lower or is_alphabet_upper
         if is_alphabet == True:
-            # step 4 , ignore capitalizaiton, force to lower case
             secret_word = secret_word.lower()
             input_user = input_user.lower()
-            # step 5, check if the letter is in secret_word
             if input_user in secret_word:
                 print(" -> Your answer is correct! . . .")
                 break
@@ -33,4 +18,3 @@ while True:
             print(" -> Input is not a valid alphabet!")
     else :
         print(" -> Please input only one letter!")
-
