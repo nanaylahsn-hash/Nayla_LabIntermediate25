d9d4fdd39f75b78c78f43c1d907469a022be0081
nayla <naylahusna@192.168.1.5>
2026-01-08 21:57:53 +0700

    update secret word

--- LabPertama_withKakaNoelGanteng.py
+++ LabPertama_withKakaNoelGanteng.py
@@ -1,20 +1,35 @@
 secret_word = "mangga"
-while True: 
-    input_user = input("Please input the letter : ").lower()
-    length_input = len(input_user)
-    if length_input == 1:
-        is_alphabet_lower = input_user >= "a" and input_user <= "z"
-        is_alphabet_upper = input_user >= "A" and input_user <= "Z"
-        is_alphabet = is_alphabet_lower or is_alphabet_upper
-        if is_alphabet == True:
-            secret_word = secret_word.lower()
-            input_user = input_user.lower()
-            if input_user in secret_word:
-                print(" -> Your answer is correct! . . .")
-                break
-            else : 
-                print(" -> Unfortunately your answer is wrong!")
-        else :
-            print(" -> Input is not a valid alphabet!")
-    else :
+guessed_letters = []
+
+while True:
+    display_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter
+        else:
+            display_word += "_"
+
+    print("Word:", display_word)
+    if display_word == secret_word:
+        print("Congratulations! You guessed the secret word!")
+        break
+
+    user_input = input("Please input a letter: ").lower()
+
+    if len(user_input) != 1:
         print(" -> Please input only one letter!")
+        continue
+
+    if not user_input.isalpha():
+        print(" -> Input is not a valid alphabet!")
+        continue
+    if user_input not in guessed_letters:
+        guessed_letters.append(user_input)
+
+        if user_input in secret_word:
+            print(" -> Correct letter!")
+        else:
+            print(" -> Wrong letter!")
+    else:
+        print(" -> You already guessed this letter!")
